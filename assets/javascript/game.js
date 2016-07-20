9c902e71ce169e0cad4b2548d1423170833c78fa
Adenike Adeyemi <adenikeadeyemi@Adenike-Adeyemis-MacBook-Pro.local>
2016-07-20 02:34:52 -0400

    finished game. May need balencing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,20 +2,21 @@
 $(document).ready(function(){
 
 	//used to create characters
-	function character(name, picture, hp){
+	function character(name, picture, hp, atk, counter){
 		this.name = name;
 		this.img = picture;
 		this.hp = hp;
 		this.status = "none";
-		this.atk = 6;
-		this.counter = 20;
+		this.atk = atk;
+		this.inc = atk;
+		this.counter = counter;
 		
 		this.damageRecieved = function(dmg){
 			this.hp -= dmg;
 		}
 
 		this.strengthen = function(){
-			this.atk += 6;
+			this.atk += this.inc;
 		}
 		this.getHp = function(){
 			return this.hp;
@@ -28,188 +29,64 @@ $(document).ready(function(){
 	var defender;
 	var defenderExist = false;
 
-	playerChoice[0] = new character("Obi-wan-Kenobi", "http://placehold.it/120x80", 120);
-	playerChoice[1] = new character("Luke Skywalker", "http://placehold.it/120x80", 120);
-	playerChoice[2] = new character("Darth Sidious", "http://placehold.it/120x80", 120);
-	playerChoice[3] = new character("Emperor Palpatine", "http://placehold.it/120x80", 120);
+	playerChoice[0] = new character("Obi-wan-Kenobi", "http://placehold.it/120x80", 120, 8, 24);
+	playerChoice[1] = new character("Luke Skywalker", "http://placehold.it/120x80", 100, 5, 5);
+	playerChoice[2] = new character("Darth Maul", "http://placehold.it/120x80", 180, 5, 25);
+	playerChoice[3] = new character("Darth Sidious", "http://placehold.it/120x80", 150, 4, 20);
 
 
 
 	//generates players
 	for(var i = 0; i < playerChoice.length; i++){
 		console.log("outside loop "+playerChoice);
-		buildCard();
+		buildCard('#characters', playerChoice[i], "character-choice", setGame);
 	
 	}//end of for loop
 
-	function setGame(){
+	function setGame(newPlayer){
+
+		player = newPlayer;
+
 		$("#characters").empty();
 
-		var card_container = $('<div>')
-		.addClass("character-card player")
-		.data("chara", player)
-		.on("click", function(){
-
-			console.log("click", $(this).data("chara"));
-			//$(this).addClass("character-player").removeClass("character-choice");
-			//player = $(this).data("chara");
-
-						
-
-		});
-
-		var card_name = $('<div>')
-			.addClass("character-name")
-			.text(player.name);
-			
-		var card_img = $('<img>')
-			.attr("src", player.img)
-			.attr("alt", "Player picture")
-
-		var card_health = $('<div>')
-			.addClass("character-health")
-			.text(player.hp)
-
-			card_container.append(card_name);
-			card_container.append(card_img);
-			card_container.append(card_health);
-			$('#player').append(card_container);
-
-
+		buildCard('#player', player, "player");
 		
-
-
 
 		//set wait array
 		for(var i = 0; i < playerChoice.length; i++){
 			if(player != playerChoice[i]){
 				
 				wait.push(playerChoice[i]);
-				var card_container = $('<div>')
-					.addClass("character-card wait")
-					.data("chara", playerChoice[i])
-					.on("click", function(){
 
-					
-					//$(this).addClass("character-player").removeClass("character-choice");
-					
-					//$('#ememies').empty();
-					console.log("flag check: " + defenderExist);
-					if(defenderExist) return;
-					console.log("click", $(this).data("chara"));
-					defender = $(this).data("chara");
-					setDefender();
-
-								
-
-				});
-
-				var card_name = $('<div>')
-					.addClass("character-name")
-					.text(playerChoice[i].name);
-					
-				var card_img = $('<img>')
-					.attr("src", playerChoice[i].img)
-					.attr("alt", "Player picture")
-
-				var card_health = $('<div>')
-					.addClass("character-health")
-					.text(playerChoice[i].hp)
-
-					card_container.append(card_name);
-					card_container.append(card_img);
-					card_container.append(card_health);
-					$('#enemies').append(card_container);
+				buildCard('#enemies', playerChoice[i], "wait", setDefender);
+				
 			}
 
 		}
-		console.log(wait);
-
-
-
-
-
-		
+		console.log(wait);		
 	}
 
-	function setDefender(){
+	function setDefender(newDefender){
+		
+		if (defenderExist) {return;}
+
+		defender = newDefender;
 		defenderExist = true;
 		$('#enemies').empty();
 
-		var card_container = $('<div>')
-		.addClass("character-card defender")
-		.data("chara", defender)
-		.on("click", function(){
-			
-			console.log("flag check: " + defenderExist);
-			console.log("click", $(this).data("chara"));
-			//$(this).addClass("character-player").removeClass("character-choice");
-			//player = $(this).data("chara");
-
-						
-
-		});
-
-		var card_name = $('<div>')
-			.addClass("character-name")
-			.text(defender.name);
-			
-		var card_img = $('<img>')
-			.attr("src", defender.img)
-			.attr("alt", "Player picture")
-
-		var card_health = $('<div>')
-			.addClass("character-health")
-			.text(defender.hp)
-
-			card_container.append(card_name);
-			card_container.append(card_img);
-			card_container.append(card_health);
-			$('#defender').append(card_container);
-
-
+		buildCard('#defender', defender, "defender");
 		
-
-
 
 		//set wait array
 
 		for(var i = 0; i < playerChoice.length; i++){
 			if(player != playerChoice[i]){
 				if(defender != playerChoice[i]){
+					if(playerChoice[i].getHp() >= 0){
 				//wait.push(playerChoice[i]);
-				var card_container = $('<div>')
-					.addClass("character-card wait")
-					.data("chara", playerChoice[i])
-					.on("click", function(){
 
-					console.log("flag check: " + defenderExist);
-					if(defenderExist) return;
-					console.log("click", $(this).data("chara"));
-					//$(this).addClass("character-player").removeClass("character-choice");
-					defender = $(this).data("chara");
-					setDefender()
-
-								
-
-				});
-
-				var card_name = $('<div>')
-					.addClass("character-name")
-					.text(playerChoice[i].name);
-					
-				var card_img = $('<img>')
-					.attr("src", playerChoice[i].img)
-					.attr("alt", "Player picture")
-
-				var card_health = $('<div>')
-					.addClass("character-health")
-					.text(playerChoice[i].hp)
-
-					card_container.append(card_name);
-					card_container.append(card_img);
-					card_container.append(card_health);
-					$('#enemies').append(card_container);
+						buildCard('#enemies', playerChoice[i], "wait", setDefender);
+					}
 				}
 			}
 
@@ -219,38 +96,39 @@ $(document).ready(function(){
 
 	}
 
-	function buildCard(){
+	function buildCard(target, value, newClass, functionOnClick){
 		var card_container = $('<div>')
-		.addClass("character-card character-choice")
-		.data("chara", playerChoice[i])
+		.addClass("character-card " + newClass)
+		.data("chara", value)
 		.on("click", function(){
 
 			console.log("click", $(this).data("chara"));
 			//$(this).addClass("character-player").removeClass("character-choice");
 			//player = $(this).data("chara");
 
-			player = $(this).data("chara");
-			setGame();
+			//player = ;
+			if(functionOnClick != undefined)
+				functionOnClick($(this).data("chara"));
 
 		});
-		console.log(playerChoice[i]);
-		console.log(playerChoice[i].name);
+		console.log(value);
+		console.log(value.name);
 		var card_name = $('<div>')
 			.addClass("character-name")
-			.text(playerChoice[i].name);
+			.text(value.name);
 			
 		var card_img = $('<img>')
-			.attr("src", playerChoice[i].img)
+			.attr("src", value.img)
 			.attr("alt", "Player picture")
 
 		var card_health = $('<div>')
 			.addClass("character-health")
-			.text(playerChoice[i].hp)
+			.text(value.hp)
 
 			card_container.append(card_name);
 			card_container.append(card_img);
 			card_container.append(card_health);
-			$('#characters').append(card_container);
+			$(target).append(card_container);
 	}
 
 	$('button').on("click", function(){
@@ -259,29 +137,58 @@ $(document).ready(function(){
 		if(defenderExist){
 
 			defender.damageRecieved(player.atk);
-			player.damageRecieved(defender.counter);
-
-			$('#player-message').text(player.name + " does " + player.atk + " damage to " + defender.name + "!");
-			$('#ememy-message').text(defender.name + " strikes back with " + defender.counter + " damage to " + player.name + "!");
-			player.strengthen();
 			
-			if(player.getHp() <= 0){
-				$('#player-message').text("You've been defeated...GAME OVER.");
-				$('#ememy-message').empty();
+			$('#player-message').text(player.name + " does " + player.atk + " damage to " + defender.name + "!");
+			
+			player.strengthen();
+
+			if(defender.getHp() <= 0){
+				$('#defender').empty();
+
+				if($('#enemies').is(':empty')) {
+					$('#player-message').text("Well Done Jedi Master you defeated all of your opponents!");
+					$('#ememy-message').empty();
+					$('#ememy-message').append($('<button>')
+						.text('Restart')
+						.on("click", function(){
+
+							location.reload();
+
+						}));
+				}
+				else{
+					$('#player-message').text("You defeated " + defender.name + ", go pick another opponent to fight!");
+					
+					$('#ememy-message').empty();
+				}
+				defenderExist = false;
+
+			}	
+			
+			if(defenderExist){
+				player.damageRecieved(defender.counter);
 				
-				$('#ememy-message').append($('<button>')
-					.text('Restart')
-					.on("click", function(){
+				$('#ememy-message').text(defender.name + " strikes back with " + defender.counter + " damage to " + player.name + "!");
 
-						$('#player-message, #defender, #player, #enemies').empty();
-						//buildCard();
-					}));
+				if(player.getHp() <= 0){
+					
+					$('#player-message').text("You've been defeated...GAME OVER.");
+					
+					$('#ememy-message').empty();
+					
+					$('#ememy-message').append($('<button>')
+						.text('Restart')
+						.on("click", function(){
 
+							location.reload();
+
+						}));
+				}
 			}
-			else if(defender.getHp() <= 0){
 
-			}
-
+									
+			
+			
 			
 
 		$('#player .character-health').text(player.hp); //update player 
